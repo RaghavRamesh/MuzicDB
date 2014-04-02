a454c355d64992235db916070dd05a0b299a820d
bryan831 <bryanchubh@gmail.com>
2014-04-02 18:19:10 +0800

    Schema: SQL DDL statements
    songs, albums, purchases now all refer to Artist by ID instead of
    artistName.

--- muzicDBddl.sql
+++ muzicDBddl.sql
@@ -12,29 +12,27 @@
 CREATE TABLE song (
 	name VARCHAR(64),		#song name
 	title VARCHAR(64),		#album title
-	artistName VARCHAR(64), 
 	artistID VARCHAR(32),
 
 	composer VARCHAR(64),   #asd ad
 	genre VARCHAR(64),
 	length INTEGER			#this will be in seconds
-	PRIMARY KEY (name, title, artistName),
-	FOREIGN KEY (title, artistName) REFERENCES album(title, artistName) ON DELETE RESTRICT ON UPDATE CASCADE,
+	PRIMARY KEY (name, title, artistID),
+	FOREIGN KEY (title, artistID) REFERENCES album(title, artistID) ON DELETE RESTRICT ON UPDATE CASCADE,
 	FOREIGN KEY (artistID) REFERENCES artist(id) ON DELETE RESTRICT ON UPDATE CASCADE 
 );                #no pri key only in ER diag, in SQL all tables will have pri key! 
 
 CREATE TABLE album (
 	title VARCHAR(64), 
-	artistName VARCHAR(64),
 	artistID VARCHAR(32),
 
-	PRIMARY KEY (title, artistName),
+	PRIMARY KEY (title, artistID),
 	FOREIGN KEY (artistID) REFERENCES artist(id) ON DELETE RESTRICT ON UPDATE CASCADE
 	/* weak entity has no primary key */
 );
 
 CREATE TABLE artist (
-	artistName VARCHAR(64),
+	artistName VARCHAR(64), NOT NULL
 	id VARCHAR(32)		PRIMARY KEY
 );
 
@@ -47,22 +45,22 @@ CREATE TABLE user (
 CREATE TABLE song_purchase (
 	name VARCHAR(64),
 	title VARCHAR(64),
-	artistName VARCHAR(64),
+	artistID VARCHAR(32),
 	price FLOAT(5,2),
 	email VARCHAR(64),
 
-	PRIMARY KEY (name, title, artistName, email),
-	FOREIGN KEY (name, title, artistName) REFERENCES song(name, title, artistName),
+	PRIMARY KEY (name, title, artistID, email),
+	FOREIGN KEY (name, title, artistID) REFERENCES song(name, title, artistID),
 	FOREIGN KEY (email) REFERENCES user(email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE album_purchase (
 	title VARCHAR(64),
-	artistName VARCHAR(64),
+	artistID VARCHAR(32),
 	price FLOAT(5,2),
 	email VARCHAR(64),
 
-	PRIMARY KEY (title, artistName, email),
-	FOREIGN KEY (title, artistName) REFERENCES album(title, artistName) ON UPDATE CASCADE ON DELETE RESTRICT,
+	PRIMARY KEY (title, artistID, email),
+	FOREIGN KEY (title, artistID) REFERENCES album(title, artistID) ON UPDATE CASCADE ON DELETE RESTRICT,
 	FOREIGN KEY (email) REFERENCES user(email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
