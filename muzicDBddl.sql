db8e329ecf8569d6cffdd8b430df6cec882cb927
bryan831 <bryanchubh@gmail.com>
2014-04-02 18:00:15 +0800

    Schema, SQL DDL statements
    2/4/2014

--- muzicDBddl.sql
+++ muzicDBddl.sql
@@ -10,23 +10,32 @@
 */
 
 CREATE TABLE song (
-	name VARCHAR(64),
-	artist VARCHAR(64),
-	length INTEGER,		/* this will be in seconds */
-	albumT VARCHAR(128), 
-	
-	PRIMARY KEY (name, artist),
-);
+	name VARCHAR(64),		#song name
+	title VARCHAR(64),		#album title
+	artistName VARCHAR(64), 
+	artistID VARCHAR(32),
+
+	composer VARCHAR(64),   #asd ad
+	genre VARCHAR(64),
+	length INTEGER			#this will be in seconds
+	PRIMARY KEY (name, title, artistName),
+	FOREIGN KEY (title, artistName) REFERENCES album(title, artistName) ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (artistID) REFERENCES artist(id) ON DELETE RESTRICT ON UPDATE CASCADE 
+);                #no pri key only in ER diag, in SQL all tables will have pri key! 
 
 CREATE TABLE album (
-	title VARCHAR(128), 
+	title VARCHAR(64), 
+	artistName VARCHAR(64),
+	artistID VARCHAR(32),
+
+	PRIMARY KEY (title, artistName),
+	FOREIGN KEY (artistID) REFERENCES artist(id) ON DELETE RESTRICT ON UPDATE CASCADE
 	/* weak entity has no primary key */
-	/* songs have one album name, or album
-	 has many song names?*/
 );
 
-CREATE TABLE song_of (
-
+CREATE TABLE artist (
+	artistName VARCHAR(64),
+	id VARCHAR(32)		PRIMARY KEY
 );
 
 CREATE TABLE user (
@@ -36,14 +45,24 @@ CREATE TABLE user (
 );
 
 CREATE TABLE song_purchase (
+	name VARCHAR(64),
+	title VARCHAR(64),
+	artistName VARCHAR(64),
+	price FLOAT(5,2),
+	email VARCHAR(64),
 
-
+	PRIMARY KEY (name, title, artistName, email),
+	FOREIGN KEY (name, title, artistName) REFERENCES song(name, title, artistName),
+	FOREIGN KEY (email) REFERENCES user(email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE album_purchase (
+	title VARCHAR(64),
+	artistName VARCHAR(64),
+	price FLOAT(5,2),
+	email VARCHAR(64),
 
-
+	PRIMARY KEY (title, artistName, email),
+	FOREIGN KEY (title, artistName) REFERENCES album(title, artistName) ON UPDATE CASCADE ON DELETE RESTRICT,
+	FOREIGN KEY (email) REFERENCES user(email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
-
-
-
